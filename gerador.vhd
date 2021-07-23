2d81c9f3919e582826537f40422efa0bac1cf0d8
luccaki <lucca.ianaguivara@gmail.com>
2021-07-23 19:56:15 -0300

    codificador done

--- gerador.vhd
+++ gerador.vhd
@@ -5,18 +5,26 @@ use ieee.std_logic_1164.all;
 entity gerador
 is port(clk : in std_logic;
  		resetn : in std_logic;
-		inputger : in std_logic;
  		seq : out std_logic
 );
 end entity gerador;
 
 architecture arqger of gerador is
+	signal package1, package2, package3 : std_logic_vector(0 to 8);
 begin
 	process
 	begin
+		package1 <= "101011100";
+		package2 <= "101001110";
+		package3 <= "001011100";
+		
+		--package1 <= "001001110";
+		--package2 <= "001101100";
+		--package2 <= "000000000";
+		
 		for i in 0 to 8 loop
 			if i=0 then
-				if inputger='0' then
+				if package1(0)='0' then
 					WAIT UNTIL (clk'EVENT AND clk = '1') ;
 					seq <= '1';
 					WAIT UNTIL (clk'EVENT AND clk = '1') ;
@@ -41,9 +49,72 @@ begin
 				end if;
 			else
 				wait until (clk'event and clk = '1') ;
-				seq <= inputger;
+				seq <= package1(i);
 			end if;
-
 		end loop;
+
+		for i in 0 to 8 loop
+			if i=0 then
+				if package2(0)='0' then
+					WAIT UNTIL (clk'EVENT AND clk = '1') ;
+					seq <= '1';
+					WAIT UNTIL (clk'EVENT AND clk = '1') ;
+					seq <= '1';
+					WAIT UNTIL (clk'EVENT AND clk = '1') ;
+					seq <= '0';
+					WAIT UNTIL (clk'EVENT AND clk = '1') ;
+					seq <= '1';
+					WAIT UNTIL (clk'EVENT AND clk = '1') ;
+					seq <= '1';
+				else
+					WAIT UNTIL (clk'EVENT AND clk = '1') ;
+					seq <= '0';
+					WAIT UNTIL (clk'EVENT AND clk = '1') ;
+					seq <= '0';
+					WAIT UNTIL (clk'EVENT AND clk = '1') ;
+					seq <= '1';
+					WAIT UNTIL (clk'EVENT AND clk = '1') ;
+					seq <= '1';
+					WAIT UNTIL (clk'EVENT AND clk = '1') ;
+					seq <= '0';
+				end if;
+			else
+				wait until (clk'event and clk = '1') ;
+				seq <= package2(i);
+			end if;
+		end loop;
+
+		for i in 0 to 8 loop
+			if i=0 then
+				if package3(0)='0' then
+					WAIT UNTIL (clk'EVENT AND clk = '1') ;
+					seq <= '1';
+					WAIT UNTIL (clk'EVENT AND clk = '1') ;
+					seq <= '1';
+					WAIT UNTIL (clk'EVENT AND clk = '1') ;
+					seq <= '0';
+					WAIT UNTIL (clk'EVENT AND clk = '1') ;
+					seq <= '1';
+					WAIT UNTIL (clk'EVENT AND clk = '1') ;
+					seq <= '1';
+				else
+					WAIT UNTIL (clk'EVENT AND clk = '1') ;
+					seq <= '0';
+					WAIT UNTIL (clk'EVENT AND clk = '1') ;
+					seq <= '0';
+					WAIT UNTIL (clk'EVENT AND clk = '1') ;
+					seq <= '1';
+					WAIT UNTIL (clk'EVENT AND clk = '1') ;
+					seq <= '1';
+					WAIT UNTIL (clk'EVENT AND clk = '1') ;
+					seq <= '0';
+				end if;
+			else
+				wait until (clk'event and clk = '1') ;
+				seq <= package3(i);
+			end if;
+		end loop;
+
+		wait;
 	end process;
 end arqger;
--- testbench.vhd
+++ testbench.vhd
@@ -13,45 +13,7 @@ architecture tb_arch of testbench is
 begin
    	-- instancia o circuito sob test (uut - unit under test)
    	uut: entity work.gerador(arqger)
-   	   port map(clk, resetn, inputger=>test_out);
-   	-- gerador dos vetores de teste
-	   
-   	process
-	   variable test1, test2, test3, test4, test5: std_logic_vector(0 to 8);
-   	begin
-		
-		test1 := "101011100";
-		test2 := "101001110";
-		test3 := "001011100";
-		test4 := "001001110";
-		test5 := "001101100";
-
-		for i in 0 to 8 loop
-			test_out <= test1(i);
-			wait for PERIOD;
-		end loop;
-
-		for i in 0 to 8 loop
-			test_out <= test2(i);
-			wait for PERIOD;
-		end loop;
-
-		for i in 0 to 8 loop
-			test_out <= test3(i);
-			wait for PERIOD;
-		end loop;
-
-		for i in 0 to 8 loop
-			test_out <= test4(i);
-			wait for PERIOD;
-		end loop;
-
-		for i in 0 to 8 loop
-			test_out <= test5(i);
-			wait for PERIOD;
-		end loop;
-      	wait;
-	end process;
+   	   port map(clk, resetn);
 
 	generate_clock: process
 	begin
@@ -60,4 +22,5 @@ begin
 		clk <= '0';
 		wait for PERIOD/2;
 	end process;
+	
 end tb_arch;
